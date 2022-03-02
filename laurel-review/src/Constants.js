eb0168d0c9f1e7e50ed7a32bcd2a9b98bd43cc9c
Raghavendra Reddy <77422478+reddy-raghavendra@users.noreply.github.com>
2022-03-01 22:31:59 -0600

    udated changes of issue

--- laurel-review/src/Constants.js
+++
@@ -1,5 +0,0 @@
-export const Constants = {
-    apis: {
-        get_issues: "https://61fe608fa58a4e00173c980a.mockapi.io/issues/issues",
-    }
-}
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+export default function Search() {
+    const [search, setSearch] = useState('')
+
+    return (
+        <div>
+            <h3 className="title">Search</h3>
+                <input 
+                    type="text" 
+                    placeholder="Search name" 
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+            />
+           
+        </div>
+    )}
